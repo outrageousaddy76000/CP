7357edd72a94b09c2bc16bcd89bbead5f160ead2
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2024-03-02 19:26:23 +0530

    Add files via upload

---
+++ start.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <time.h>
+#include <stdlib.h>
+
+//----------------------------------------------------------------
+#ifndef ONLINE_JUDGE
+#include "template.cpp"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+//----------------------------------------------------------------
+
+
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(),v.end()
+#define endl '\n'
+#define getunique(v) {sort(v.begin(),v.end());v.erase(unique(v.begin(),v.end()),v.end());}
+#define getunique1(v) {v.erase(unique(v.begin(),v.end()),v.end());}
+
+
+const int mod = 1000000007;
+//const int mod = 998244353;
+
+long double tick(){static clock_t oldt; clock_t newt=clock();
+    long double diff = 1.0L*(newt-oldt)/CLOCKS_PER_SEC;oldt = newt; return diff;}
+
+void fast_io(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return;
+}
+
+signed main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int n;
+        cin>>n;
+        
+    }
+    return 0;
+}
